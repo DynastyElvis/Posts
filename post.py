3c48fc734dec1914b0183b694a0ea67bbc67f13e
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-14 16:54:38 +0300

    ft: added user class and post classes

--- post.py
+++ post.py
@@ -1,8 +1,42 @@
+from email.policy import default
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
+
+
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 app.config['SECRET_KEY'] = '123456789'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+class User (db.Model): # User class
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    email = db.Column(db.String(60), nullable=False)
+    
+    
+class Post(db.Model):# Post class
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(300), unique=True, nullable=False)
+    #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    date = db.Column(db.String(60), nullable=False,  default=datetime.utcnow)     
+    
+    
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 
