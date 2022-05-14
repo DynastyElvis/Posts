bfc306fbf73fa93e8f4fc850d291483e5339a948
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-14 18:20:08 +0300

    ft: migrations and user classes updates

--- post/models.py
+++ post/models.py
@@ -6,16 +6,20 @@ class User (db.Model): # User class
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    avator = db.Column(db.String(20), nullable=False, default='default.png')
     email = db.Column(db.String(60), nullable=False)
+    post = db.relationship('Post', backref='author', lazy=True)
     
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.avator}')"
     
 class Post(db.Model):# Post class
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     content = db.Column(db.String(300), unique=True, nullable=False)
     #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    date = db.Column(db.String(60), nullable=False,  default=datetime.utcnow)     
+    date = db.Column(db.String(60), nullable=False,  default=datetime.utcnow) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)    
     
     
     
