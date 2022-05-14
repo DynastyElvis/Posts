67de0e338d2a7a192fe604f1e56bbe28648cddb6
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-14 18:31:08 +0300

    ft: flask login

--- post/__init__.py
+++ post/__init__.py
@@ -1,12 +1,12 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
+from flask_login import LoginManager
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
 
 app.config['SECRET_KEY'] = '123456789'
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
